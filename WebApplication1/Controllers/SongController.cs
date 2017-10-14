cd949894eebe75c779119f3ae2452130ac2a4168
bryanhmiller <bryankalos@hotmail.com>
2017-10-14 10:39:02 -0500

    Moving local repo to github

---
+++ WebApplication1/Controllers/SongController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SongController : Controller
+    {
+        public ActionResult GrabData(Song song)
+        {
+            return RedirectToAction("Create");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+    }
+}
